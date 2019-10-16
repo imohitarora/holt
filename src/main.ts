36ccfb2a5bd8ce6cbed7d8f7ffd53957a649045a
Mohit Arora <mohit.arora@selfinindia.com>
2019-10-16 11:32:01 +0530

    redis microservice

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Transport } from '@nestjs/microservices';
+import { microserviceOptions } from './config/options/microservice.options';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(AppModule, {
-    transport: Transport.TCP,
-  });
+  const app = await NestFactory.createMicroservice(AppModule, microserviceOptions);
   app.listen(() => console.log('Microservice is listening'));
-
-  // const app = await NestFactory.create(AppModule);
-  // await app.listen(3000);
 }
 bootstrap();
